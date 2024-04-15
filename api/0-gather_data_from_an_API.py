29b3a36baa7a24ccbc7cc9f485ebde72c4d47aeb
Mottais <mottais.afy@outlook.fr>
2024-04-15 15:48:57 +0200

    Test autre solution

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     id = argv[1]
     url_api = 'https://jsonplaceholder.typicode.com'
-    json_user = requests.get(f'{url_api}/users/{id}')
+    json_user = requests.get(url_api + "/users/" + id)
     json_todo = requests.get(f'{url_api}/todos?userId={id}')
 
     data_user = json_user.json()
