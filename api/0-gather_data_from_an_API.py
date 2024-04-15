888f772c4241d0b11b4cd0e1b27fb09c81d13cf9
Mottais <mottais.afy@outlook.fr>
2024-04-15 15:16:39 +0200

    Task0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""get data from an api"""
+import requests
+from sys import argv
+
+
+id = argv[1]
+
+if __name__ == "__main__":
+    url_api = 'https://jsonplaceholder.typicode.com'
+    json_user = requests.get(url_api + "/users/" + id)
+    json_todo = requests.get(url_api + "/todos?userId=" + id)
+
+    if json_user.status_code != 200:
+        exit()
+
+    data_user = json_user.json()
+    data_todos = json_todo.json()
+
+    fait = 0
+    for todo in data_todos:
+        if todo.get('completed'):
+            fait += 1
+
+    str = "Employee {} ".format(data_user.get('name'))
+    str = str + "is done with tasks({}/{}):".format(fait, len(data_todos))
+
+    print(str)
+    for todo in data_todos:
+        if todo.get('completed'):
+            print("\t " + todo.get('title'))
